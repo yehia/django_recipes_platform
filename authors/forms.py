6caad9e26c5de9346a102e8fb3a07951ed0770e3
Yehia Azanki <yazanki@gmai.com>
2022-06-08 04:47:55 -0300

    Create initial test author register

--- authors/forms.py
+++ authors/forms.py
@@ -1,5 +1,8 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
@@ -11,6 +14,19 @@ def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
 
 
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
+
+
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,29 +34,26 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
-        add_attr(self.fields['username'], 'css', 'a-css-class')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
 
     password = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Your password'
-        }),
+        widget=forms.PasswordInput(),
         error_messages={
-            'required': 'Password must not be empty'
+            'required': 'Password must not be empty',
         },
         help_text=(
-            'Password must have at least one uppercase letter,'
-            'one lowercase letter and one number. The length should be'
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
 
     password2 = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Repeat your password'
-            })
+        widget=forms.PasswordInput()
     )
 
     class Meta:
@@ -72,11 +85,46 @@ class RegisterForm(forms.ModelForm):
                 'required': 'This field must not be empty'
             }
         }
-        widgets = {
-            'first_name': forms.TextInput(
-                attrs={'placeholder': 'Type your first name here'}
-            ),
-            'password': forms.PasswordInput(
-                attrs={'placeholder': 'Type your password here'}
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+        if 'John Doe' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo first_name',
+                code='invalid',
+                params={'value': '"John Doe"'}
             )
-        }
+
+        return data
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo password',
+                code='invalid',
+                params={'value': '"atenção"'}
+            )
+
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                'Password and password2 must be equal',
+                code='invalid'
+            )
+
+            raise ValidationError({
+                'password': password_confirmation_error,
+                'password2': [
+                    password_confirmation_error,
+                    'Another error',
+                ],
+            })
