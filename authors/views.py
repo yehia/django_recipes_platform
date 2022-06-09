7877fe2cfbcfea2f647f2d79904c045601ba7a98
Yehia Azanki <yazanki@gmail.com>
2022-06-09 01:28:25 -0300

    Organizing files

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from .forms import RegisterForm
 
@@ -11,6 +12,7 @@ def register_view(request):
 
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:create'),
     })
 
 
@@ -23,7 +25,9 @@ def register_create(request):
     form = RegisterForm(POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user is created, please log in.')
 
         del(request.session['register_form_data'])
