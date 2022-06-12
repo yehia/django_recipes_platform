ac31bce0fd7ce84b34be4277845fd3e689f11f6e
Yehia Azanki <yazanki@gmail.com>
2022-06-12 09:57:09 -0300

    Django debug Toolbar

--- recipes/urls.py
+++ recipes/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('recipes/category/<int:category_id>/',
          views.RecipeListViewCategory.as_view(), name="category"),
     path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
+    path('recipes/api/v1/', views.RecipeListViewHomeAPI.as_view(),
+         name="recipe_api_v1"),
+    path('recipes/api/v1/<int:pk>/', views.RecipeDetailAPI.as_view(),
+         name="recipe_api_v1_detail"),
 ]
